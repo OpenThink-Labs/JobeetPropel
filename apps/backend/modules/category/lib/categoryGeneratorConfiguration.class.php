9b243cd86d93d7e78b6a0a7fb3c762c344cf8553
Wildan Maulana <wildan.m@openthinklabs.com>
2013-03-24 11:39:10 +0700

    Hari 12

---
+++ apps/backend/modules/category/lib/categoryGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * category module configuration.
+ *
+ * @package    jobeet
+ * @subpackage category
+ * @author     Wildan Maulana, OpenThink Labs
+ * @version    SVN: $Id: configuration.php 12474 2008-10-31 10:41:27Z fabien $
+ */
+class categoryGeneratorConfiguration extends BaseCategoryGeneratorConfiguration
+{
+}
